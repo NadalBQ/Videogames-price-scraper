e617e5a09f453dcd0a87d0fbe451e1eead7818c9
NadalBQ <nadalbq@gmail.com>
2024-12-25 20:50:00 +0100

    Steam_scraper ready to scrape steam in search for games retrieving prices and storing at a steamGames.json file

--- steam_scraper.py
+++ steam_scraper.py
@@ -1,3 +1,4 @@
+import json
 from aids import *
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -11,21 +12,6 @@ driver.implicitly_wait(10)
 
 categories = ["singleplayer", "multiplayer_mmo", "multiplayer", "multiplayer_local_party", "multiplayer_lan", "multiplayer_coop", "multiplayer_online_competitive"]
 
-def scroll(driver: Chrome):
-    SCROLL_PAUSE_TIME = 0.2
-    last_height = driver.execute_script("return document.body.scrollHeight")
-    while True:
-        # Scroll down to bottom
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-        # Wait to load the page
-        wait(SCROLL_PAUSE_TIME)
-
-        # Calculate new scroll height and compare with last scroll height
-        new_height = driver.execute_script("return document.body.scrollHeight")
-        if new_height == last_height:
-            break
-        last_height = new_height
 
 def getPageGames(driver: Chrome, games={}):
     elements = driver.find_elements(by=By.CLASS_NAME, value='gASJ2lL_xmVNuZkWGvrWg')
@@ -55,7 +41,7 @@ def mostrarMas(driver: Chrome):
     driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", button)
     wait(2)
     if button.is_displayed() and button.is_enabled():
-        button_position = button.location
+        
         button.click()
         driver.execute_script("""scrollBy({top:-10000000000000000, left:0, behavior: "smooth"});""")
         wait(2)  # Esperar un poco para cargar más elementos
@@ -63,17 +49,16 @@ def mostrarMas(driver: Chrome):
         print("Botón 'Mostrar más' no disponible.")
     
 
-dic_videojuegos = {"jurasic Part": [[(15, "Epic"), (16, "Steam")], 8.5, 40]}
-
-# Inicio del proceso
 a = start()
 games = {}
 
 i = 0
 link_template = "https://store.steampowered.com/category/{category}/?offset={offset}"
 for category in categories:
-    
-    while i < 100:
+
+    last_len = len(games)
+    while True:
+
         driver.get(link_template.format(category=category, offset=i))
         i += 12
         wait(100, "ms")
@@ -89,13 +74,15 @@ for category in categories:
         print("gettin games!")
         games = getPageGames(driver, games)
         print(len(games))
+        if last_len == len(games):
+            break
+        last_len = len(games)
 
 b = finish(a)
 print("*" * 100, "\n" * 5)
 # print(games)
 print(f"{b} segundos")
 
-import json
 
-with open("games.json", "w+") as outfile:
+with open("steamGames.json", "w+") as outfile:
     json.dump(games, outfile)
