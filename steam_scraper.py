c54c1e86f93f1118d97d84ce6f9a2923cbd216e4
NadalBQ <nadalbq@gmail.com>
2024-12-29 22:21:22 +0100

    Made redundant every steam_scraper file other than this one, the original with the shortest form of its name. "steam_scraper.py"

--- steam_scraper.py
+++ steam_scraper.py
@@ -7,9 +7,6 @@ from selenium.webdriver import Chrome
 
 
 
-categories = ["singleplayer", "multiplayer_mmo", "multiplayer", "multiplayer_local_party", "multiplayer_lan", "multiplayer_coop", "multiplayer_online_competitive"]
-
-
 def getPageGames(driver: Chrome, games={}):
     elements = driver.find_elements(by=By.CLASS_NAME, value='gASJ2lL_xmVNuZkWGvrWg')
     for element in elements:
@@ -21,7 +18,8 @@ def getPageGames(driver: Chrome, games={}):
             print(f"Error al procesar un elemento: {e}")
     return games
 
-def steam_scrape():
+
+def steam_multiple_scrape(categories=["singleplayer", "multiplayer_mmo", "multiplayer", "multiplayer_local_party", "multiplayer_lan", "multiplayer_coop", "multiplayer_online_competitive"]):
     # Cambia el driver según el navegador que uses
     driver = webdriver.Edge()
     # driver = webdriver.Chrome()
@@ -74,8 +72,75 @@ def steam_scrape():
 
     b = finish(t)
     print("*" * 100, "\n" * 5)
-    # print(games)
+
+    dump_into(games, "steamGames")
     print(f"{b} segundos")
 
 
-    dump_into(games, "steamGames")
+def steam_category_scrape(category: str):
+    # Cambia el driver según el navegador que uses
+    driver = webdriver.Edge()
+    # driver = webdriver.Chrome()
+    driver.implicitly_wait(3)
+    t = start()
+    games = {}
+
+    i = 0
+    last_len = 0
+    link_template = "https://store.steampowered.com/category/{category}/?offset={offset}"
+    
+    last_len = len(games)
+
+    # Para el scrape de "singleplayer", que tiene muchos juegos, merece la pena dividir la tarea en varios procesos paralelos que comiencen en una i distinta.
+    '''
+    first = int(input("Desde qué i quieres empezar? "))
+    i = first
+    a = 0
+    twelve = True
+    while True and i < first + 60000:
+    '''
+
+    i = 0
+    a = 0
+    twelve = True
+    while True:
+
+        driver.get(link_template.format(category=category, offset=i))
+        # i += 12
+        wait(100, "ms")
+        driver.execute_script("""scrollBy({top:10000000000000000, left:0, behavior: "smooth"});
+                        let elem = document
+                        .querySelector("._1cOoCFwafBlSkwllIMf3XM._1FPIVJTLsw1nvAN24BGGKg.SaleSectionTabs");
+                        
+                        if (elem) {
+                        elem.remove();
+                        }
+                        """)
+
+        print("gettin games!")
+        games = getPageGames(driver, games)
+        print("games:", len(games), "a:", a, "i:", i)
+        if len(games) != last_len:
+            a = 0
+        if a == 0:
+            twelve = True
+        else:
+            twelve = False
+        
+        if twelve:
+            i += 12
+        else:
+            i += 1
+
+        if a > 144:
+            break
+        elif last_len == len(games):
+            a += 1
+        last_len = len(games)
+
+    b = finish(t)
+    print("*" * 100, "\n" * 5)
+    
+    dump_into(games, f"steamGames{category}")
+    print(f"{b} segundos")
+
